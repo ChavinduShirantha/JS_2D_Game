d894cfc6b8bea3fcd803bfc0776acd8f9f10d6d4
ChavinduShirantha <chavindushirantha@gmail.com>
2023-07-12 12:37:27 +0530

    Added Game Instructions

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$('#intro-icon').on('click', function (e) {
+    $('#intro').attr('style', 'display : block !important');
+});
+$('#close').on('click', function (e) {
+    $('#intro').attr('style', 'display : none !important');
+});
