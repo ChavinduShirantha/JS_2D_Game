febcd92b0235fdfe89d77fb9202a84be52bca2c8
ChavinduShirantha <chavindushirantha@gmail.com>
2023-07-12 12:59:13 +0530

    Added audio

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ $('#intro-icon').on('click', function (e) {
 $('#close').on('click', function (e) {
     $('#intro').attr('style', 'display : none !important');
 });
+
+const theme =new Audio("assets/audio/Theme2.mp3");
+
+window.addEventListener('mousemove', handleMouseMove);
+function handleMouseMove(event) {
+    theme.play();
+}
+
