773542903413340fa960f1b8ee0da61f430f10d1
ChavinduShirantha <chavindushirantha@gmail.com>
2023-07-16 11:15:18 +0530

    Creating moving road

---
+++ assets/js/levelOneScript.js
@@ -0,0 +1,19 @@
+let roadPositionY = 0;
+let moveRoadAnimationId = 0;
+
+function moveRoad() {
+    clearInterval(moveRoadAnimationId);
+
+    moveRoadAnimationId = setInterval(function () {
+        roadPositionY = roadPositionY + 20;
+        $("#moveRoad").css("background-position-y", +roadPositionY + "px");
+    }, 100);
+}
+
+$(document).on('keypress', function (e) {
+    if (e.keyCode === 32) {
+        if (moveRoadAnimationId === 0) {
+            moveRoad();
+        }
+    }
+});
