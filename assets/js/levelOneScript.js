33afb7eb438f11a6cb9a898169a7c54493e43894
ChavinduShirantha <chavindushirantha@gmail.com>
2023-07-16 16:07:55 +0530

    Added Function Buttons

--- assets/js/levelOneScript.js
+++ assets/js/levelOneScript.js
@@ -18,3 +18,25 @@ $(document).on('keypress', function (e) {
         }
     }
 });
+
+$('#btnSoundOff').on('click', function (e) {
+    $('#btnSoundOn').attr('style', 'display : block !important');
+    $('#btnSoundOff').attr('style', 'display : none !important');
+});
+$('#btnSoundOn').on('click', function (e) {
+    $('#btnSoundOff').attr('style', 'display : block !important');
+    $('#btnSoundOn').attr('style', 'display : none !important');
+});
+
+function pauseAll() {
+    clearInterval(moveRoadAnimationId);
+    moveRoadAnimationId = 0;
+}
+
+$('#btnPauseGame').on('click', function (e) {
+    pauseAll();
+});
+
+$('#btnPlayGame').on('click', function (e) {
+    moveRoad();
+});
