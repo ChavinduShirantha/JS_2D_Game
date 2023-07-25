d5af53615ebfc5954db1896ca0f10883b6c25889
ChavinduShirantha <chavindushirantha@gmail.com>
2023-07-25 16:38:39 +0530

    Added Game Over Functions

--- assets/js/levelOneScript.js
+++ assets/js/levelOneScript.js
@@ -3,6 +3,8 @@ let moveRoadAnimationId = 0;
 let score = 0;
 
 let sound = new Audio("../assets/audio/Theme2.mp3");
+let victory = new Audio("../assets/audio/Victory.mp3");
+let lose = new Audio("../assets/audio/Lose.wav");
 
 
 function moveRoad() {
@@ -13,7 +15,7 @@ function moveRoad() {
         $("#moveRoad").css("background-position-y", +roadPositionY + "px");
         score++;
         $("#score").text(score);
-        if (score >= 500) {
+        if (score >= 100) {
             winResults();
         }
     }, 100);
@@ -24,6 +26,7 @@ $(document).on('keypress', function (e) {
         if (moveRoadAnimationId === 0) {
             moveRoad();
             $('#startup').attr('style', 'display : none !important');
+            $('#enemyCar').attr('style', 'display : block !important');
             sound.play();
             sound.loop=true;
         }
@@ -47,6 +50,7 @@ function pauseAll() {
     clearInterval(moveRoadAnimationId);
     moveRoadAnimationId = 0;
     sound.pause();
+    $('#enemyCar').attr('style', 'display : none !important');
 }
 
 function blurComponents() {
@@ -69,12 +73,15 @@ $('#btnPlayGame').on('click', function (e) {
     sound.play();
     sound.muted=false;
     removeBlur();
+    $('#enemyCar').attr('style', 'display : block !important');
 });
 
 function winResults() {
     $("#gameWin_title-img").css("display", "block");
+    $('#enemyCar').attr('style', 'display : none !important');
     pauseAll();
     blurComponents();
+    victory.play();
 }
 
 let myCar = document.getElementById("car");
@@ -151,3 +158,23 @@ window.addEventListener("load", function () {
 
     loader.style.display = 'none';
 });
+
+function defeat(){
+    $("#gameOverWrapper").css("display", "block");
+    pauseAll();
+    blurComponents();
+    lose.play();
+}
+
+setInterval(function GameOver() {
+    var enemyCarTop = parseInt(window.getComputedStyle(enemyCar).getPropertyValue("top"))
+    var enemyCarLeft = parseInt(window.getComputedStyle(enemyCar).getPropertyValue("left"));
+    var myCarLeft = parseInt(window.getComputedStyle(myCar).getPropertyValue("left"));
+    if ((enemyCarLeft === myCarLeft) && (enemyCarTop > -250)) {
+        clearInterval(moveRoadAnimationId);
+        moveRoadAnimationId = 0;
+        defeat();
+        $("#enemyCar").css("display", "none");
+        $("#btnTryAgain").css("display", "block");
+    }
+}, 10)
